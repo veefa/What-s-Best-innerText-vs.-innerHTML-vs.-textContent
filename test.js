cc38bfe0d073597ecfd6031e24ac7ed465e167c3
vefa <44091688+veefa@users.noreply.github.com>
2021-10-08 12:12:22 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,5 @@
+
+const getValue = document.getElementById("blog-test");
+console.log("textContent:",getValue.textContent);
+console.log("innerText:", getValue.innerText);
+console.log("innerHTML:", getValue.innerHTML);
