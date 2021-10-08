696874b53ec220b969b8c8de1deb03bd9177c8d9
vefa <44091688+veefa@users.noreply.github.com>
2021-10-08 12:25:02 +0100

    Update test.js
    innerText retrieves and sets the content of the tag as plain text.
    innerHTML retrieves and sets the same content in HTML format.
    The content of all elements in the node, including script and style elements. It’s aware of formatting like spacing and line breaks and will return those, innerText is very similar to textContent, however, there are important differences between them! Put simply, innerText is aware of the rendered appearance of text, while textContent is not.

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
-
+  
+(<div id='blog-test'>
+  <p>This element is <strong>strong</strong> and     has some super fun <code>code</code>!</p>
+</div>);
+ 
 const getValue = document.getElementById("blog-test");
 console.log("textContent:",getValue.textContent);
 console.log("innerText:", getValue.innerText);
 console.log("innerHTML:", getValue.innerHTML);
+//retutn : 
+/* textContent: 
+          This element is strong and     has some super fun code!
+innerText: 
+          This element is strong and has some super fun code!
+innerHTML:
+          <p>This element is <strong>strong</strong> and     has some super fun <code>code</code>!</p>
+        */
